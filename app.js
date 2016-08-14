c0885c16b9bcbcdec3a4b9ff6f030aa7df0d3215
Administrator <majid.sadeghi.alavijeh@gmail.com>
2016-08-15 01:23:54 +0430

    Add some logs

--- app.js
+++ app.js
@@ -92,6 +92,6 @@ server.start(function (err) {
     if (err) {
         throw err;
     }
-    console.log('Server running at:', server.info.uri);
+    console.log('Server running at:', server.info);
 })
 
