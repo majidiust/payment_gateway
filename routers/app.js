a6e3a305e442a298be38e78134b8ea7745e28583
Administrator <majid.sadeghi.alavijeh@gmail.com>
2016-08-15 00:34:54 +0430

    Add some test

--- routers/app.js
+++ routers/app.js
@@ -3,34 +3,6 @@ var AppController = require("../controller/app").ApplicationController;
 function ApplicationRouter() {
     function registerRoutes(server) {
         server.route({
-            method: 'GET',
-            path: '/getPaymentGateways',
-            config: {auth: false},
-            handler: PaymentController().getPaymentGateway
-        });
-
-        server.route({
-            method: 'GET',
-            path: '/getAllPayments',
-            config: {auth: false},
-            handler: PaymentController().getListOfPayment
-        });
-
-        server.route({
-            method: 'GET',
-            path: '/IPGCallback',
-            config: {auth: false},
-            handler: PaymentController().IPGCallback
-        });
-
-        server.route({
-            method: 'GET',
-            path: '/paymentTestCallback',
-            config: {auth: false},
-            handler: PaymentController().paymentTestCallback
-        });
-
-        server.route({
             method: 'POST',
             path: '/signupAdmin',
             config: {auth: false},
